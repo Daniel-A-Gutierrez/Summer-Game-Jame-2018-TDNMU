4eafd13f7868abdf4de0ce71bf0d94523864d6aa
ConfusedSky <masamaedae@gmail.com>
2018-08-18 14:56:58 -0700

    Adds Attack Patern interface
    * Also adds a library that I will be using in the future to create
      curves in the game.

---
+++ Assets/AttackPatterns/Types/TestPattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TestPattern : AttackPattern
+{
+    public string InitialMessage = "Hello World";
+    public int WaitTime = 10;
+
+    public override IEnumerator SequenceCoroutine(MonoBehaviour runner, Action callback)
+    {
+        Debug.Log(InitialMessage);
+        for(int i = 0; i < WaitTime; i++)
+        {
+            Debug.Log(i);
+            yield return new WaitForSeconds(1.0f);
+        }
+        callback();
+    }
+}
