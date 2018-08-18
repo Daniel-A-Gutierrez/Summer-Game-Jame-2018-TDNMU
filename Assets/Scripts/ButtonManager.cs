40db3c61e740cac6369b232079c4fec1ee7c057f
CodiakVII <codiak759@gmail.com>
2018-08-18 16:03:38 -0700

    Main Menu Framework

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject controlsPage;
+
+    public void NewGameBtn()
+    {
+        SceneManager.LoadScene("Main Level");
+    }
+
+    public void credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+	public void controls()
+	{  
+		controlsPage.SetActive (true);
+	}
+
+	public void controlsExit()
+	{
+		controlsPage.SetActive (false);
+	}
+
+    public void quitToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene("Main Level");
+    }
+}
