20401e83cda26a995c23efb0be01b4832265df81
Daniel-A-Gutierrez <icanmakespaghetti@gmail.com>
2018-08-19 13:22:05 -0700

    Sound

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
 
+[System.Serializable]
 public class Sound : MonoBehaviour {
 
+	public AudioClip clip;
+	public string name;
+	[Range(0f,1f)]
+	public float volume;
+	[Range(.1f,8f)]
+	public float pitch;
+
+	[HideInInspector]
+	public AudioSource source;
 	// Use this for initialization
 	void Start ()
 	{
