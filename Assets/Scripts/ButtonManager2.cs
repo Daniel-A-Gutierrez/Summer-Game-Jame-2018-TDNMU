6d263ed75f129aa452cf30771a237c618abec56e
CodiakVII <codiak759@gmail.com>
2018-08-19 16:19:56 -0700

    Main Level 99 Percent Done

---
+++ Assets/Scripts/ButtonManager2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager2 : MonoBehaviour
+{
+    AudioSource controlsMusic;
+    AudioSource AS;
+
+    public void NewGameBtn()
+    {
+        StartCoroutine("StartGame");
+    }
+
+    public void quitToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene("Main Level");
+    }
+}
