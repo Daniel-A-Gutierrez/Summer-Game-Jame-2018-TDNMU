ad5994bf26feeb97b76ec9d6ee1fc2a09c8b1da1
ConfusedSky <masamaedae@gmail.com>
2018-08-19 16:11:53 -0700

    Creates the cone composer.
    * Fixes the rotate actions.
    * Adds a lot of new AttackPatterns.

---
+++ Assets/AttackPatterns/Types/Composers/Cone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Attack Pattern/Composers/Cone")]
+public class Cone : AttackPattern
+{
+    [Expandable]
+    public AttackPattern Pattern;
+    public int RepeatCount;
+    public int Angle;
+
+    private int wait;
+    public override IEnumerator SequenceCoroutine(MonoBehaviour runner, Action callback,
+        Vector3? positionOffset = default(Vector3?), Quaternion? rotationOffset = default(Quaternion?))
+    {
+        wait = 0;
+        float anglePerRepeat = (float)Angle / RepeatCount;
+        runner.transform.Rotate(new Vector3(0, 0, -1), -(float)Angle / 2 + anglePerRepeat / 2);
+        for (int i = 0; i < RepeatCount; i++)
+        {
+            runner.StartCoroutine(Pattern.SequenceCoroutine(runner, internalCallback, positionOffset, rotationOffset));
+            runner.transform.Rotate(new Vector3(0, 0, -1), anglePerRepeat);
+        }
+        runner.transform.Rotate(new Vector3(0, 0, -1), -(float)Angle / 2 - anglePerRepeat / 2);
+        yield return new WaitUntil(() => wait >= RepeatCount - 1);
+        callback();
+    }
+
+    private void internalCallback()
+    {
+        wait++;
+    }
+}
--- Assets/AttackPatterns/Types/Rotation/Rotate.cs
+++ Assets/AttackPatterns/Types/Rotation/Rotate.cs
@@ -6,20 +6,13 @@ using UnityEngine;
 class Rotate : AttackPattern
 {
     public float Angle;
-    public float Speed;
 
     public override IEnumerator SequenceCoroutine(MonoBehaviour runner, Action callback,
         Vector3? positionOffset = default(Vector3?), Quaternion? rotationOffset = default(Quaternion?))
     {
-        float frameSpeed = Speed * Time.fixedDeltaTime;
-        float frameCount = Angle / frameSpeed;
-
-        for (int i = 0; i < frameCount; i++)
-        {
-            runner.transform.Rotate(new Vector3(0, 0, frameSpeed));
-            yield return new WaitForFixedUpdate();
-        }
+        runner.transform.Rotate(new Vector3(0, 0, 1), Angle);
         callback();
+        yield return null;
     }
 }
 
