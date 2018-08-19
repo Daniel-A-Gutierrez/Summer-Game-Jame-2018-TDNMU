1492193ff5b7b3c5eda5bbfe1cf8148c6ccd2804
Daniel-A-Gutierrez <icanmakespaghetti@gmail.com>
2018-08-19 15:56:43 -0700

    FUllly FUNCTIONAL MENU

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,10 +5,33 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     public GameObject controlsPage;
-
+    AudioSource controlsMusic;
+    AudioSource AS;
     public void NewGameBtn()
     {
+
+        StartCoroutine("StartGame");
+    }
+
+    void Start()
+    {
+        controlsMusic = gameObject.GetComponent<AudioSource>();
+        AS = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+    }
+
+    IEnumerator StartGame()
+    {
+        
+        float volume = AS.volume;
+        float duration = 1.5f;
+        float start = Time.time;
+        while(Time.time-start < duration)
+        {
+            AS.volume*=.95f;
+            yield return null;
+        }
         SceneManager.LoadScene("Main Level");
+
     }
 
     public void credits()
@@ -16,14 +39,45 @@ public class ButtonManager : MonoBehaviour
         SceneManager.LoadScene("Credits");
     }
 
+    IEnumerator FadeInMusic()
+    {
+
+        float duration = 2.0f;
+        float start = Time.time;
+        while(Time.time-start < duration)
+        {
+            if(controlsMusic.volume<.98f)
+            {
+                controlsMusic.volume+=1/120f;
+                AS.volume *=.91f;
+            }
+            yield return null;
+        }
+        controlsPage.SetActive (true);
+    }
+
+    IEnumerator FadeOutMusic()
+    {
+        float duration = 2.0f;
+        float start = Time.time;
+        while(Time.time-start < duration)
+        {
+            AS.volume/=.91f;
+            controlsMusic.volume*= .93f;
+            yield return null;
+        }
+        controlsPage.SetActive (false);
+
+    }
+
 	public void controls()
 	{  
-		controlsPage.SetActive (true);
+		StartCoroutine("FadeInMusic");
 	}
 
 	public void controlsExit()
 	{
-		controlsPage.SetActive (false);
+		StartCoroutine("FadeOutMusic");
 	}
 
     public void quitToMenu()
--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -25,7 +25,7 @@ public class Credits : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        music = gameObject.GetComponent<AudioSource>();
+        //music = gameObject.GetComponent<AudioSource>();
         started = false;
 
         if (textFile != null)
@@ -80,6 +80,7 @@ public class Credits : MonoBehaviour
 
     IEnumerator waitThreeSeconds()
     {
+        StartCoroutine(FadeInMusic());
         yield return new WaitForSeconds(3.0f);
         theText.text = "Programmers:\n\nMasa Maeda\n\nDaniel Gutierrez\n\nUlises Perez";
         yield return new WaitForSeconds(6.0f);
@@ -90,7 +91,34 @@ public class Credits : MonoBehaviour
         InvokeRepeating("FadeToBlack", 0.0f, 0.02f);
         yield return new WaitForSeconds(3.0f);
         yield return new WaitForSeconds(3.0f);
-        yield return new WaitForSeconds(1.0f);
-        SceneManager.LoadScene("Main Menu");
+        StartCoroutine("FadeOutMusic");
     }
+
+    IEnumerator FadeInMusic()
+    {
+    float duration = 2.0f;
+    float start = Time.time;
+    while(Time.time-start < duration)
+    {
+        if(music.volume<.98f)
+        {
+            music.volume+=1/120f;
+        }
+        yield return null;
+    }
+    }
+
+    IEnumerator FadeOutMusic()
+    {
+        float duration = 2.0f;
+        float start = Time.time;
+        while(Time.time-start < duration)
+        {
+            music.volume*=.93f;
+            yield return null;
+        }
+        SceneManager.LoadScene("Main Menu");
+
+    }
+    
 }
